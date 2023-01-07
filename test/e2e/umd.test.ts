183df8ddc2a14ee7f7840f247e8aaded010f876d
k_senda <k_senda@yumemi.co.jp>
2023-01-07 16:07:46 +0900

    fix: split test script: unit/e2e

--- test/e2e/umd.test.ts
+++ test/e2e/umd.test.ts
@@ -1,6 +1,3 @@
-/**
- * @jest-environment puppeteer
- */
 import { join } from "path"
 
 describe("load umd from HTML", () => {
@@ -9,16 +6,12 @@ describe("load umd from HTML", () => {
     const output: string[] = []
     page.on("console", (m) => output.push(m.text()))
 
-    console.log(__dirname)
-
     const path = `file://${join(__dirname, "index.html")}`
-    console.log(path)
 
     await page.goto(path, {
       waitUntil: "networkidle0"
     })
 
-    console.log(output)
     expect(output.length).toBe(3)
     expect(output[0]).toBe("敬老の日")
     expect(output[1]).toBe("true")
--- test/unit/global-setup.js
+++ test/unit/global-setup.js
@@ -1,6 +1,3 @@
 module.exports = async () => {
-    // 1. set timezone
     process.env.TZ = "Asia/Tokyo"
-    // 2. setup puppeteer
-    await require("jest-environment-puppeteer/setup")()
 }
