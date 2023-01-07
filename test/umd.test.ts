77b886ad97803534601980878e624762f42bf131
k_senda <k_senda@yumemi.co.jp>
2023-01-07 15:22:10 +0900

    fix: wait js run before verify

--- test/umd.test.ts
+++ test/umd.test.ts
@@ -7,9 +7,10 @@ describe("load umd from HTML", () => {
   test("check console output", async () => {
     const output: string[] = []
     page.on("console", (m) => output.push(m.text()))
-    await page.goto(`file://${__dirname}/index.html`, {
-      waitUntil: "networkidle0"
-    })
+    await Promise.all([
+      page.goto(`file://${__dirname}/index.html`),
+      page.waitForFunction("window.status === 'done'")
+    ]) 
 
     expect(output.length).toBe(3)
     expect(output[0]).toBe("敬老の日")
