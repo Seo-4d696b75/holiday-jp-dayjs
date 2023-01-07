dc0b1ddf3da7eb9bba65375446d668b97095e136
k_senda <k_senda@yumemi.co.jp>
2023-01-07 15:14:26 +0900

    fix: umd test

--- test/umd.test.ts
+++ test/umd.test.ts
@@ -4,17 +4,12 @@
 
 describe("load umd from HTML", () => {
 
-
-  beforeAll(async () => {
-    // await page.goto('https://google.com')
-
-    //await page.goto(`file://${__dirname}/index.html`)
-  })
-
   test("check console output", async () => {
     const output: string[] = []
     page.on("console", (m) => output.push(m.text()))
-    await page.goto(`file://${__dirname}/index.html`)
+    await page.goto(`file://${__dirname}/index.html`, {
+      waitUntil: "domcontentloaded"
+    })
 
     expect(output.length).toBe(3)
     expect(output[0]).toBe("敬老の日")
